ab3bf7c7f303f502b3e0ed82f27599561e259e97
shahramsiwaily <55696920+Shahram-tech@users.noreply.github.com>
2024-03-06 16:25:48 +0300

    ...

--- app.py
+++ app.py
@@ -6,24 +6,26 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
 
 # Load Airbnb dataset
-airbnb_data = pd.read_csv("d.csv")
+airbnb_data = pd.read_csv("Airbnb_Data.csv")
 
 # Initialize OpenAI language model
-llm = OpenAI(temperature=0, openai_api_key="api-key")
+llm = OpenAI(temperature=0, openai_api_key="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")
 memory = ConversationBufferMemory(llm=llm, max_token_limit=100)
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/')
 def index():
   return render_template('index.html')
+
 
 @app.route('/data', methods=['POST'])
 def get_data():
   data = request.get_json()
   user_input = data.get('data')
-  
+
   try:
     # Example conversation handling
     if "price" in user_input.lower():
@@ -35,13 +37,17 @@ def get_data():
       conversation = ConversationChain(llm=llm, memory=memory)
       response = conversation.predict(input=user_input)
       memory.save_context({"input": user_input}, {"output": response})
-  
+
+    # Appending a message
+    response += "As Real Estate AI How may I assist you further?"
+
     return jsonify({"response": True, "message": response})
-  
+
   except Exception as e:
     print(e)
     error_message = f'Error: {str(e)}'
     return jsonify({"message": error_message, "response": False})
 
+
 if __name__ == '__main__':
-  app.run()
+  app.run(host="0.0.0.0", port=8080)
